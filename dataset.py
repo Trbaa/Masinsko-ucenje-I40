6544587f44cdc56fb0149dd1789f12cb6750579c
Dusan <trba@Trbaas-mac.local>
2026-03-09 14:52:01 +0100

    Added comments

--- dataset.py
+++ dataset.py
@@ -5,6 +5,10 @@ import tensorflow as tf
 
 
 print(tf.__version__)
+
+#Učitavanje  slike
+#Dekodiranje slike u tenzor sa 3 kanala (Red Green Blue)
+#Konverzija slika u tip float32
 def load_image(path):
 
     tf_read = tf.io.read_file(path)
@@ -14,7 +18,8 @@ def load_image(path):
 
     return tf_img_float
     
-
+#Kreiranje isečka visoke rezolucije (hr)
+#Ukoliko je slika manja od željenog okvira povećavamo je bilinearnom interpolacijom
 def make_hr_patch(img,hr_size):
     shape = tf.shape(img)
     h = shape[0]
@@ -37,6 +42,9 @@ def make_hr_patch(img,hr_size):
     hr_patch = tf.image.random_crop(img, size=[hr_size,hr_size,3])
     return hr_patch
 
+
+#Od slike visoke rezolucije pravimo sliku niske rezolucije
+#Smanjivanje rezolucije se radi bicubic metodom
 def make_lr_patch(hr, scale):
     h = tf.shape(hr)[0]
     w = tf.shape(hr)[1]
@@ -49,6 +57,8 @@ def make_lr_patch(hr, scale):
 
     return lr_small
 
+
+#Pravimo par slika visoke i niske rezolucije kako bi se nad njima kasnije trenirao model
 def make_pair(path,hr_size,scale):
     img = load_image(path)
     hr = make_hr_patch(img, hr_size)
@@ -56,6 +66,15 @@ def make_pair(path,hr_size,scale):
 
     return lr,hr
 
+
+#Pravimo tenzor set sa slikama iz path-a
+#Ukoliko je trening set mešamo podatke
+#Glavna logika je linija:
+#ds= ds.map(lambda p: make_pair(p,hr_size,scale),num_parallel_calls = tf.data.AUTOTUNE)
+#Za svaku učitanu sliku, pravi se par lr i hr
+#Za trening situaciju: ukoliko nema dovoljno parova za poslednji batch, prihvatamo ga iako je manji
+#Za test situaciju: ukoliko nema dovoljno parova za poslednji batch, odbacujemo ih
+#Prefetch eliminiše usko grlo
 def make_sr_dataset(paths,hr_size,scale,batchs_size,training):
     shuffle_buffer = 1000
 
@@ -71,7 +90,7 @@ def make_sr_dataset(paths,hr_size,scale,batchs_size,training):
     else:
         ds = ds.batch(batchs_size,drop_remainder = False)
 
-    ds = ds.prefetch(tf.data.AUTOTUNE)
+    ds = ds.prefetch(tf.data.AUTOTUNE) 
 
         
     return ds
